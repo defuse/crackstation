29f7fc588dd740548c3d82ce6ffb45bf6f0281b7
Taylor Hornby <havoc@defuse.ca>
2012-06-09 14:13:35 +0000

    added number format to cs hits.php

--- src/hits.php
+++ src/hits.php
@@ -1,7 +1,7 @@
 <?php
     require_once('libs/hitcounter.php');
     require_once('libs/phpcount.php');
-    echo "t: " . PHPCount::GetTotalHits();
-    echo " u: " . PHPCount::GetTotalHits(true);
-    echo " c: " . htmlspecialchars(getCrackedCount() . " of " . getCrackAttemptCount());
+    echo "t: " . number_format(PHPCount::GetTotalHits(), 0);
+    echo " u: " . number_format(PHPCount::GetTotalHits(true), 0);
+    echo " c: " . htmlspecialchars(number_format(getCrackedCount(), 0) . " of " . number_format(getCrackAttemptCount(), 0));
 ?>
