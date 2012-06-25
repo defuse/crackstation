9eaa216448c0e067f2e422ff921d7a578c983634
Taylor Hornby <havoc@defuse.ca>
2012-06-25 00:31:27 +0000

    fixed aboutus and contact crackstation pages

--- src/pages/aboutus.php
+++ src/pages/aboutus.php
@@ -1,1 +1,28 @@
-<p>Information about us will be posted here soon!</p>
+<div class="box">
+<div class="padding">
+<h3>About CrackStation</h3>
+
+<p>
+CrackStation is a security awareness project started by <a href="https://defuse.ca/">Defuse
+Cyber-Security</a>. Its purpose is to raise awareness about insecure password storage in web
+applications, and to provide guidance to implementors of user authentication systems. By making
+large hash lookup tables freely available to the public, we make it easier for security researchers
+to demonstrate why password storage solutions, like non-salted hashing, are insecure.
+</p>
+
+<p>
+While CrackStation does provide paid services, its goal is not to make a profit. The money is used
+to pay for the hardware required to run CrackStation, and any left-overs go toward other security
+research projects (the results of which are released into the public domain). You may support
+CrackStation and other Defuse Cyber-Security projects by donating using the PayPal donate button
+above.
+</p>
+
+<p>
+If you have any questions, comments, or concerns about CrackStation, or would like help implementing
+secure password storage in an authentication system, please contact us. You can find
+our contact information on the <a href="/contact-us.htm">Contact Information Page</a>.
+</p>
+
+</div>
+</div>
--- src/pages/contactus.php
+++ src/pages/contactus.php
@@ -1,1 +1,10 @@
-<p>A ticket system is coming soon. In the meantime, email crackstation@defuse.ca.</p>
+<div class="box">
+<div class="padding">
+<h3>Contacting CrackStation</h3>
+<p>
+To contact CrackStation, send an email to <a href="mailto:crackstation@defuse.ca"><strong>crackstation@defuse.ca</strong></a>
+We'll be glad to answer any questions you have, and we appreciate all feedback, good or bad. You
+should receive a reply within two days.
+</p>
+</div>
+</div>
