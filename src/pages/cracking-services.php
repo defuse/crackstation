fc7bec67c25d4d74811a7822d6a14723911172ba
Taylor Hornby <havoc@defuse.ca>
2013-01-13 04:49:18 +0000

    - close crackstation cracking services

--- src/pages/cracking-services.php
+++ src/pages/cracking-services.php
@@ -1,63 +1,10 @@
-<!--<div class="box">
-    <div class="padding">
-        Rainbow Tables | Bulk Cracking | Salted Hash Cracking | WPA Cracking | Archive file cracking | Miscellaneous
-    </div>
-</div>-->
-
-<div class="box">
-<div class="padding">
 <h1>Advanced Password Cracking Services</h1>
 <h2>Salted Hash, WPA, WEP, UNIX, Linux, MySQL, and Rainbow Table Hash Cracking</h2>
 
-<p>CrackStation provides you with easy, inexpensive access to our massively effective password cracking dictionaries. 
-Prices are listed below in Canadian Dollars.</p>
+<p>
+CrackStation is no longer providing custom hash cracking services. There is too much manual effort
+involved, and I feel that my time would be better spent on other projects. I may re-open the service
+after I have automated it, but the types of jobs we received are so diverse that automation is
+incredibly challenging. So it may not happen.
+</p>
 
-<table cellspacing="20" class="packages">
-    <tr>
-        <th style="text-align: left;">
-            Hash Type
-        </th>
-        <th style="text-align: left;">
-            Dictionary
-        </th>
-        <th style="text-align: left;">
-            Price
-        </th>
-    </tr>
-    <tr>
-        <td>Bulk Unsalted</td><td>Medium</td><td>$5 + ($0.05 per 1000 hashes)</td>
-    </tr>
-    <tr>
-        <td>&nbsp;</td><td>Huge</td><td>$5 + ($0.10 per 1000 hashes)</td>
-    </tr>
-    <tr>
-        <td>Salted Single-Iteration</td><td>Medium</td><td>$5.00</td>
-    </tr>
-    <tr>
-        <td>&nbsp;</td><td>Huge</td><td>$10.00</td>
-    </tr>
-    <tr>
-        <td>Salted Multi-Iteration (e.g. PBKDF2, crypt)</td><td>Medium</td><td>$15.00</td>
-    </tr>
-    <tr>
-        <td>&nbsp;</td><td>Huge</td><td>$30.00</td>
-    </tr>
-    <tr>
-        <td>WPA</td><td>Medium</td><td>$15.00</td>
-    </tr>
-    <tr>
-        <td>&nbsp;</td><td>Huge</td><td>$30.00</td>
-    </tr>
-    <tr>
-        <td>LanMan (LM)</td><td>ASCII 1-14chrs Rainbow Table</td><td>$10.00</td>
-    </tr>
-    <tr>
-        <td>NTLM</td><td>Rainbow Table</td><td>$10.00</td>
-    </tr>
-</table>
-<br />
-<p>Please email orders to <a href="mailto:crackstation@defuse.ca">crackstation@defuse.ca</a> (<a href="/contact-us.htm">public key</a>). We accept PayPal and BitCoin payments. Prices are in Canadian Dollars (CAD).</p>
-
-<p>Please note that because we do the same amount of work, we must ask for payment regardless of whether the hash is cracked or not. We will, however, only charge 50% of the prices listed here if we cannot crack your password, to give us incentive to maintain the best password cracking dictionaries on the Internet. When we can't crack it, you are paying for the knowledge that the password cannot be cracked by the one of the best password cracking dictionaries on the Internet.</p>
-</div>
-</div>
