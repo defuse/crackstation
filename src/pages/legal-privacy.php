384e458eabf262982372ecf098c4de151dc19aa0
Taylor Hornby <havoc@defuse.ca>
2013-02-18 14:52:56 +0000

    remove bad text from cracksatation tos

--- src/pages/legal-privacy.php
+++ src/pages/legal-privacy.php
@@ -5,7 +5,7 @@ CrackStation's main goal is to promote the use of properly implemented salted
 hashing in new and existing web applications. We provide this service to help
 security researchers demonstrate the importance of hash salting. Any users that
 choose to use CrackStation as a malicious tool are solely responsible for their
-own actions. We do not ask (and do not care) where our clients get their hashes,
+own actions. We do not ask where our clients get their hashes,
 so we are unable to help any law enforcement agency.
 </p>
 <h2>Privacy Policy</h2>
