15f82f30e6fd00e87f76e405991676744a54a1b4
Taylor Hornby <havoc@defuse.ca>
2013-11-05 11:43:54 -0700

    Fix the privacy policy.

--- src/pages/legal-privacy.php
+++ src/pages/legal-privacy.php
@@ -1,16 +1,21 @@
 <h1>CrackStation's Terms of Service and Privacy Policy</h1>
+
 <h2>Terms of Service</h2>
 <p>
 CrackStation's main goal is to promote the use of properly implemented salted
 hashing in new and existing web applications. We provide this service to help
 security researchers demonstrate the importance of hash salting. Any users that
 choose to use CrackStation as a malicious tool are solely responsible for their
-own actions. We do not ask where our clients get their hashes,
-so we are unable to help any law enforcement agency.
+own actions. We do not ask where our clients get their hashes, so we are unable
+to help any law enforcement agency.
 </p>
+
 <h2>Privacy Policy</h2>
 <p>
-CrackStation respects the privacy of its users. Our Privacy Policy is simple: We
-do not keep ANY logs. Your IP address (and any other PII) is NOT saved for any
-period of time. We do not even cache hash lookups.
+CrackStation respects the privacy of its users. Your IP address may be logged as
+you request web pages from this site, but we do not sell that information or
+share that information with anyone. We DO NOT log the hashes that you crack.
+However, you should still not try to crack the hash of your own password or
+other sensitive information, since your connection might be being intercepted by
+the NSA.
 </p>
