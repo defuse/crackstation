9e47256a7f636dc3c4435855b2ded0bb53a541e1
Taylor Hornby <havoc@defuse.ca>
2015-05-04 10:48:57 -0600

    Change email addr and fix typo

--- src/pages/contactus.php
+++ src/pages/contactus.php
@@ -3,15 +3,16 @@
 <h1>Contacting CrackStation</h1>
 
 <p>
-    <strong>Please Note:</strong> I have stopped replying to email. If you would
-    have feedback or a question, and want to receive a reply, please tweet it to
-    me instead. I will very rarely reply to any email, but I do enjoy reading
-    everything that gets sent to me.
+    <strong>Please Note:</strong> I currently do not have time to reply to
+email. If you have feedback or a question, and want to receive a reply, please
+tweet it to me instead. I will very rarely reply to any email, but I do enjoy
+reading everything that gets sent to me.
 </p>
 
 <p>
-To contact CrackStation, send an email to <a href="mailto:crackstation@defuse.ca"><strong>crackstation@defuse.ca</strong></a>
+To contact CrackStation, send an email to <img src="images/email.gif" alt="havoc_remove_this_@_remove_this_defuse.ca" />
 </p>
+
 </div>
 
 <h2>PGP Public Key</h2>
