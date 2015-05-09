2c7775de88640c3a0d1a91b07127374613520108
Taylor Hornby <havoc@defuse.ca>
2015-05-09 13:12:23 -0600

    Add email exception to crackstation

--- src/pages/contactus.php
+++ src/pages/contactus.php
@@ -3,10 +3,15 @@
 <h1>Contacting CrackStation</h1>
 
 <p>
-    <strong>Please Note:</strong> I currently do not have time to reply to
+<strong>Please Note:</strong> I currently do not have time to reply to
 email. If you have feedback or a question, and want to receive a reply, please
 tweet it to me instead. I will very rarely reply to any email, but I do enjoy
 reading everything that gets sent to me.
+</p>
+
+<p>
+<b>Note:</b> If you need support for the wordlist or a payment, please do send
+me an email.
 </p>
 
 <p>
