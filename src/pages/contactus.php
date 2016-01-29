e4a60d830c7daa5cfb6626e7b4fbe5af711a3965
Taylor Hornby <havoc@defuse.ca>
2016-01-29 13:00:27 -0700

    Make contact us page link to defuse.ca's page.

--- src/pages/contactus.php
+++ src/pages/contactus.php
@@ -3,19 +3,10 @@
 <h1>Contacting CrackStation</h1>
 
 <p>
-<strong>Please Note:</strong> I currently do not have time to reply to
-email. If you have feedback or a question, and want to receive a reply, please
-tweet it to me instead. I will very rarely reply to any email, but I do enjoy
-reading everything that gets sent to me.
-</p>
-
-<p>
-<b>Note:</b> If you need support for the wordlist or a payment, please do send
-me an email.
-</p>
-
-<p>
-To contact CrackStation, send an email to <img src="images/email.gif" alt="havoc_remove_this_@_remove_this_defuse.ca" />
+If you purchased the wordlist and it isn't working for you, or otherwise need
+support with the website, or just want to provide feedback, please email me. You
+can find my contact information
+<a href="https://defuse.ca/contact.htm">here</a>.
 </p>
 
 </div>
