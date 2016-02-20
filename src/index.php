133941e6eb8eedbde657832b07bbca02660513b4
Taylor Hornby <taylor@defuse.ca>
2016-02-20 06:33:08 -0700

    Remove VimHighlight dependency.

--- src/index.php
+++ src/index.php
@@ -38,28 +38,6 @@ file_put_contents("/dev/urandom", $entropy);
 require_once('libs/URLParse.php'); 
 require_once('libs/hitcounter.php');
 require_once('libs/phpcount.php');
-require_once('libs/VimHighlight.php');
-
-function printHlString($text, $ft, $numbers = false) {
-    $hl = new VimHighlight();
-    $hl->caching = true;
-    $hl->color_scheme = "dw_cyan";
-    $hl->show_lines = $numbers;
-    $hl->use_css = true;
-    $hl->file_type = $ft;
-    $hl->setVimCommand("vim");
-    echo '<div class="vimhighlight">' . $hl->processText($text, true) . '</div>';
-}
-
-function printSourceFile($path, $numbers = false) {
-    $hl = new VimHighlight();
-    $hl->caching = true;
-    $hl->color_scheme = "dw_cyan";
-    $hl->show_lines = $numbers;
-    $hl->use_css = true;
-    $hl->setVimCommand("vim");
-    echo '<div class="vimhighlight">' . $hl->processFile($path, true) . '</div>';
-}
 
 $name = URLParse::ProcessURL();
 PHPCount::AddHit($name, $_SERVER['REMOTE_ADDR']);
@@ -90,7 +68,6 @@ header('Content-Type: text/html; charset=utf-8');
 <link rel="stylesheet" media="all" type="text/css" href="/css/mainmenu2.css" />
 <link rel="stylesheet" media="all" type="text/css" href="/css/main.css" />
 <link rel="stylesheet" media="all" type="text/css" href="/css/style.css" />
-<link rel="stylesheet" media="all" type="text/css" href="/css/vimhl.css" />
 <!--[if !IE 7]>
 	<style type="text/css">
 		#wrap {display:table;height:100%}
